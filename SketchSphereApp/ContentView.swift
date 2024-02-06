0272fe847105178010243fb42cef624b7e4819d7
madomaru <melondoka1502@gmail.com>
2024-02-06 16:11:05 +0900

    Initial Commit

---
+++ SketchSphereApp/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  SketchSphereApp
+//
+//  Created by 伊藤まどか on 2024/02/06.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ SketchSphereApp/SketchSphereAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  SketchSphereAppApp.swift
+//  SketchSphereApp
+//
+//  Created by 伊藤まどか on 2024/02/06.
+//
+
+import SwiftUI
+
+@main
+struct SketchSphereAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
